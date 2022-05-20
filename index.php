3a6f50ef6dd98cbac50cc52d2beed5e79a813056
Miriamfernandez <miriamfernandez@estudiante.edib.es>
2022-05-20 16:30:36 +0200

    Pasando html a php

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 
-    //AQUI SE EJECUTA TODA LA APP
+//AQUI SE EJECUTA TODA LA APP
 
-    #require_once() -> requiere un documento una unica vez.
-    require_once('./modelo/Conexion.php');
-    require_once('./modelo/UsuarioModelo.php');
-    
-    require_once('./controlador/ControladorPlantilla.php');
-    require_once('./controlador/ControladorRegistro.php');
-    require_once('./controlador/ControladorLogin.php');
-    
+#require_once() -> requiere un documento una unica vez.
+require_once('./modelo/Conexion.php');
+require_once('./modelo/UsuarioModelo.php');
 
-    //1) MANERA ESTATICA
-    ControladorPlantilla::traerPlantilla();
+require_once('./controlador/ControladorPlantilla.php');
+require_once('./controlador/ControladorRegistro.php');
+require_once('./controlador/ControladorLogin.php');
+
+
+//1) MANERA ESTATICA
+ControladorPlantilla::traerPlantilla();
 
     //2) MANERA CONVENCIONAL
     /*
---
+++ vistas/paginas/login.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Página Log in</title>
+    <!--CSS Link-->
+    <link rel="stylesheet" href="./css/login.css" />
+    <!--Agregar link JQuery para enseñar/esconder Form -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <div id="loader"></div>
+
+    <div class="container">
+        <div class="form">
+            <div class="btn">
+                <button class="registrarseBtn">Registrate</button>
+                <button class="LoginBtn">Inicia Sesión</button>
+            </div>
+            <form action="" class="iniciaSesion">
+                <div class="formGroup">
+                    <input type="text" name="Nombre" id="nombre_usuario" placeholder="Nombre Usuario" autocomplete="off" />
+                </div>
+                <div class="formGroup">
+                    <input type="email" name="Email" id="Email ID" placeholder="Email" autocomplete="off" />
+                </div>
+                <div class="formGroup">
+                    <input type="password" name="contraseña" id="contraseña" placeholder="contraseña" autocomplete="off" />
+                </div>
+                <div class="formGroup">
+                    <input type="confirmaConstraseña" name="confirmaciónContraseña" id="confirmaConstraseña" placeholder="Confirmar Contraseña" autocomplete="off" />
+                </div>
+                <!--Check Box-->
+                <div class="checkBox">
+                    <input type="checkbox" name="checkbox" id="checkbox" />
+                    <span class="texto">
+                        ESTOY DE ACUERDO CON LOS TERMINOS Y CONDICIONES</span>
+                </div>
+                <div class="formGroup">
+                    <a href="./index.html" class="btn2">Unirme</a>
+                </div>
+            </form>
+
+            <!--Creación parte Iniciar Sesión-->
+            <form action="" class="LogIn">
+                <div class="formGroup">
+                    <input type="email" name="Email" id="Email ID" placeholder="Email" autocomplete="off" />
+                </div>
+                <div class="formGroup">
+                    <input type="password" name="contraseña" id="contraseña" placeholder="contraseña" autocomplete="off" />
+                </div>
+                <!--Check Box-->
+                <div class="checkBox">
+                    <input type="checkbox" name="checkbox" id="checkbox" />
+                    <span class="texto">
+                        MANTENDEME INFORMADO DE NUEVOS PRODUCTOS POR EMAIL</span>
+                </div>
+                <div class="formGroup">
+                    <a href="../plantilla.php" class="btn2">Iniciar Sesión</a>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!--Link javscript-->
+    <script src="./js/script.js"></script>
+    <?php
+    $ctrRegistro = new ControladorRegistro();
+    $ctrRegistro->registrarse();
+
+    ?>
+
+
+</body>
+
+</html>
